b5d9a790bf0a56b4adba4f8c3a74cecb392864a9
“zksgit config --global user.name 'zks'git config --global user.name “zks <zks13218919255@163.com>
2023-12-01 17:27:27 +0800

    个人中心

--- src/components/libirary/xtx-tabs.vue
+++ src/components/libirary/xtx-tabs.vue
@@ -1,14 +1,78 @@
-
-<script  lang="jsx">
-import { h } from 'vue'
+<script lang="jsx">
+import { useVModel } from '@vueuse/core'
+import { provide, ref } from 'vue'
 export default {
-    name:'XtxTabs',
-    render(){
-        return <h1>s</h1>
+    name: 'XtxTabs',
+    // jsx语法，它能够让我们创建节点和写html一样
+    // 1. 动态插值表达式{} 2. 尽量三元表示式做判断，使用map来遍历 3.事件使用原始方式绑定
+    // 默认插槽节点
+    props: {
+        modelValue: {
+            type: [Number, String],
+            default: 0
+        }
+
+    },
+    setup(props, { emit }) {
+
+        const toggle = (name) => {
+            emit('update:modelValue', name)
+        }
+        return { toggle }
+    },
+    render() {
+        const panels = this.$slots.default();
+        console.log(panels);
+        // 支持动静结合 v-for渲染和直接渲染
+        const content = []
+        panels.forEach(item => {
+            // 静态 v-for的初始渲染
+            if (item.type.name === "XtxTabsPanel") {
+                content.push(item)
+            } else {
+                item.children.forEach(ele => {
+                    // console.log(ele);
+                    content.push(ele)
+                });
+
+            }
+        })
+
+        const nav = <nav>{
+            content.map((item, index) => {
+                return <a class={{active:item.props.name===this.modelValue}} href='javascript:;' onClick={() => this.toggle(item.props.name)}>{item.props.label}</a>
+            })
+        }
+        </nav>
+
+
+        return <div class={'xtx-tabs'}>{[nav, panels]}</div>
     }
 }
-
 </script>
+<style scoped lang="scss">
+.xtx-tabs {
+    background: #fff;
 
+    >nav {
+        height: 60px;
+        line-height: 60px;
+        display: flex;
+        border-bottom: 1px solid #f5f5f5;
 
-<style lang='less' scoped></style>
+        >a {
+            width: 110px;
+            border-right: 1px solid #f5f5f5;
+            text-align: center;
+            font-size: 16px;
+
+            &.active {
+                border-top: 2px solid $xtxColor;
+                height: 60px;
+                background: #fff;
+                line-height: 56px;
+            }
+        }
+    }
+}
+</style>
--- src/views/member/user/order.vue
+++ src/views/member/user/order.vue
@@ -1,12 +1,27 @@
 <template>
-    <XtxTabs />
   <router-link to="/member/order/26">订单详情</router-link>
+
+  <XtxTabs v-model="activeName">
+
+    <XtxTabsPanel label="选项卡0" name="first0">
+      我是内容0
+    </XtxTabsPanel>
+
+
+    <XtxTabsPanel :label="`选项卡${item}`" :name="`first${item}`" v-for="item in 3" :key="item">
+      我是内容{{ item }}
+    </XtxTabsPanel>
+
+    <XtxTabsPanel label="选项卡4" name="first4">
+      我是内容4
+    </XtxTabsPanel>
+  </XtxTabs>
 </template>
 <script setup >
+import XtxTabsPanel from '@/components/xtx-tabs-panel.vue';
 import { ref, reactive } from 'vue'
+const activeName=ref('first0')
 </script>
 
 
-<style lang='scss' scoped>
-
-</style>
+<style lang='scss' scoped></style>
